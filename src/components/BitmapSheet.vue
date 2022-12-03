c5a4527b987e1070305e1e1951fc5eeb6ddfafb6
osawa-koki <github@osawa.dev>
2022-12-03 13:41:58 +0900

    Uint8Arrayの取得まで。

--- src/components/BitmapSheet.vue
+++ src/components/BitmapSheet.vue
@@ -4,7 +4,7 @@
     alt="import"
     @click="($refs['file'] as HTMLInputElement).click()"
   />
-  <input type="file" class="hidden" ref="file" onchange="onFileChange" />
+  <input type="file" class="hidden" ref="file" v-on:change="onFileChange" />
   <div id="BitmapSheet"></div>
 </template>
 
@@ -19,8 +19,29 @@ export default defineComponent({
   methods: {
     onFileChange(e: any) {
       const files = e.target.files || e.dataTransfer.files;
-      if (!files.length) return;
-      this.createImage(files[0]);
+      if (files.length < 0) return;
+      const file = files[0];
+      this.file2ByteArray(file)
+        .then((data) => {
+          const byte_array = new Uint8Array(data);
+          console.log(byte_array);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    file2ByteArray(file: File): Promise<Uint8Array> {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = (event: any) => {
+          if (event === null) {
+            reject("event is null");
+            return;
+          }
+          resolve(event.target.result);
+        };
+        reader.readAsArrayBuffer(file);
+      });
     },
     createImage(file: any) {
       const image = new Image();
